5eb2b23677264ad6a61556c4b1acc598497a2ef9
LMSerhii <Leonovserhii89@gmail.com>
2023-10-08 14:53:51 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,50 @@
-"use strict";
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
 
-const yearOfBirth = 1989;
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
 
-console.log("I was born in", yearOfBirth);
+//   addPotion(newPotion) {
+//     for (const potion of this.potions) {
+//       if (potion.name === newPotion.name) {
+//         return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//       }
+//     }
 
-const message = "What happend duck";
+//     this.potions.push(newPotion);
+//   },
 
+//   removePotion(potionName) {
+//     for (const potion of this.potions) {
+//       if (potionName === potion.name) {
+//         this.potions.splice(this.potions.indexOf(potion), 1);
+//         return `Potion ${potionName} succesfully deleted!`;
+//       }
+//     }
 
-// const isComing = confirm("Please confirm hotel reservation");
+//     return `Potion ${potionName} is not in inventory!`;
+//   },
 
-// console.log(isComing);
+//   updatePotionName(oldName, newName) {
+//     for (const potion of this.potions) {
+//       if (oldName === potion.name) {
+//         potion.name = newName;
+//         return `Potion succesfully updated!`;
+//       }
+//     }
+//     return `Potion ${oldName} is not in inventory!`;
+//   },
+//   // Change code above this line
+// };
 
-const hotelName = prompt("Please confirm hotel reservation");
+// console.log(
+//   atTheOldToad.updatePotionName("Stone skin", "Invulnerability potion")
+// );
 
-console.log(hotelName);
+// console.log(atTheOldToad.getPotions());
